4c419dfe9a45fe064cbef1f1f52a5eb9a0a2385a
Clemy-beep <clementine.digny@gmail.com>
2022-06-29 09:50:13 +0200

    new authorized address in CORS

--- back/index.js
+++ back/index.js
@@ -10,7 +10,7 @@ var users = require("./routes/user");
 var films = require("./routes/films");
 
 app.use(express.json());
-app.use(cors({ origin: ["http://localhost:8080"] }));
+app.use(cors({ origin: ["http://localhost:8081", "http://localhost:80"] }));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 
